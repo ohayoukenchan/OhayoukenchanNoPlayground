5cc2a20bb3fd45bd707754d18ca1ba67e4e05acc
kenta.takano <san.kun.sakana@gmail.com>
2020-04-10 13:44:57 +0900

    Initial Commit

---
+++ OhayoukenchanNoPlayground/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OhayoukenchanNoPlayground
+//
+//  Created by  ohayoukenchan on 2020/04/10.
+//  Copyright © 2020  ohayoukenchan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
